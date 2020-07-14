169a8e9f1b7af9a6606f4182ff96f86249098c9f
twishabansal <58483338+twishabansal@users.noreply.github.com>
2020-07-14 20:32:03 +0530

    Create DecodeString.cpp

---
+++ Assignments/DecodeString.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> digits(string s){
+    vector<int> ans = {1};
+    int count = 0;
+    for (char const &c : s){
+        if (c == 'I'){
+            ans.push_back(ans.size() + 1);
+            count = 0;
+        }
+        else{
+            count ++;
+            int tmp = ans.back();
+            for (int i = 0; i < ans.size() ; i++){
+                cout << ans[i] << endl;
+            }
+            cout << count<< endl;
+            for (int i = ans.size() - 1; i > ans.size() - 1 - count; i--){
+                ans[i] ++;
+            }
+            for (int i = 0; i < ans.size() ; i++){
+                cout << ans[i] << endl;
+            }
+            ans.push_back(tmp);
+        }
+    }
+   return ans; 
+}
+
+int main(){
+    string s;
+    cin >> s;
+    vector<int> ans = digits(s);
+    for (int i = 0; i < ans.size(); i++){
+        cout << ans[i];
+    }
+    return 0;
+}
