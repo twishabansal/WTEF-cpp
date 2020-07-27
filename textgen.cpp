3d830d00f042cd65e55036d54012f0ae247cec62
Twisha Bansal <twishabansal07@gmail.com>
2020-07-27 09:08:56 +0530

    new

--- textgen.cpp
+++ textgen.cpp
@@ -13,12 +13,14 @@ string make_word(int how_long) {
     return word;
 }
 
-unordered_set<string> words(string filename){
+unordered_set<string> words(string filename, int word_len){
     unordered_set<string> words;
     ifstream sowpods(filename);
     string word;
     while (sowpods >> word){
+        if (word.length() == word_len){
         words.insert(word);
+        }
     }
     return words;
 }
@@ -26,7 +28,7 @@ unordered_set<string> words(string filename){
 
 int main(int argc, char* argv[]) {
     const int word_len = stoi(argv[1]);
-    unordered_set <string> all_words = words("sowpods.txt");
+    unordered_set <string> all_words = words("sowpods.txt", word_len);
     int words_reqd = 10, count = 0, iter = 0;
     string word;
     while (count < words_reqd) {
